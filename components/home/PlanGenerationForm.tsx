256b4fd1b33b5765f90357f33a672772da3bc8f1
Kevin Haxhi <kevin.haxhi@softup.co>
2023-03-30 15:39:36 +0200

    [FEAT] Removed old stuff from layout and set up PlanGenerationForm component

---
+++ components/home/PlanGenerationForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlanGenerationForm: React.FC = () => {
+  return (
+    <h1 className="mx-auto mb-5 max-w-4xl font-display text-4xl font-bold tracking-normal sm:text-6xl">
+      PlanGenerationForm
+    </h1>
+  );
+};
+
+export default PlanGenerationForm;
