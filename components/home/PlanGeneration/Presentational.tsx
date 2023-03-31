ccd530683264878d78fd6212668fb80f9584b927
Kevin Haxhi <kevin.haxhi@softup.co>
2023-03-31 14:17:53 +0200

    Implemented basic plan generation form

--- components/home/PlanGeneration/Presentational.tsx
+++ components/home/PlanGeneration/Presentational.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
+import PlanGenerationForm from './components/PlanGenerationForm/Presentational';
+
 const PlanGeneration: React.FC = () => {
   return (
     <div id="generate" className="min-h-full py-16 plan-generation-container container">
       <h1 className="text-center mx-auto mb-5 max-w-4xl font-display text-4xl font-bold tracking-normal sm:text-6xl">
-        PlanGeneration
+        Create your personalized meal plan
       </h1>
       <h2 className="text-center mx-auto mb-4 max-w-xl text-lg leading-7 text-section-subtitle">
-        Sub header
+        Enter your information and a list of ingredients to get started
       </h2>
+      <PlanGenerationForm />
     </div>
   );
 };
