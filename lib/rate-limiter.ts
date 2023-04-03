ff93f63e6ea5ff461139570faab27d4cdd721630
Kristi <kristi.kristo@softup.co>
2023-04-03 15:42:38 +0200

    added rate limiter

---
+++ lib/rate-limiter.ts
@@ -0,0 +1,13 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import redis from "@/lib/redis";
+
+// Allow 3 requests, every 60 seconds
+const ratelimit = redis
+  ? new Ratelimit({
+      redis: redis,
+      limiter: Ratelimit.fixedWindow(3, "60 s"),
+      analytics: false,
+    })
+  : undefined;
+
+export default ratelimit;
--- pages/api/ingredientsImage.ts
+++ pages/api/ingredientsImage.ts
@@ -1,24 +1,23 @@
-import {
-  NextApiRequest,
-  NextApiResponse,
-} from 'next';
-import Replicate from 'replicate';
+import { NextApiRequest, NextApiResponse } from "next";
+import Replicate from "replicate";
+import requestIp from "request-ip";
 
-import { ImageRequestType } from '@/lib/types';
-import { prepareImagePromptForRequest } from '@/lib/utils';
-import { imageDataValidationSchema } from '@/lib/validation';
+import { ImageRequestType } from "@/lib/types";
+import { prepareImagePromptForRequest } from "@/lib/utils";
+import { imageDataValidationSchema } from "@/lib/validation";
+import rateLimit from "@/lib/rate-limiter";
 
 export const requestToReplicateEndPoint = async (
   preparedPrompt: string,
   numInferenceSteps: number,
   index?: number,
 ) => {
-
   const replicate = new Replicate({
     auth: process.env.REPLICATE_API_KEY || "",
   });
 
-  const model = "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf";
+  const model =
+    "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf";
   const input = {
     prompt: preparedPrompt,
     num_inference_steps: numInferenceSteps,
@@ -27,25 +26,43 @@ export const requestToReplicateEndPoint = async (
   const output: any = await replicate.run(model, { input });
 
   return { imageUrl: output[0], index };
-}
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     try {
       const userDataObject: ImageRequestType = JSON.parse(req.body);
-      const isImageDataValid = imageDataValidationSchema.safeParse(userDataObject);
+      const isImageDataValid =
+        imageDataValidationSchema.safeParse(userDataObject);
 
       if (!isImageDataValid.success) {
         const errorMessage = isImageDataValid.error.issues[0].message;
         res.status(400).json({
           error: {
             message: errorMessage,
-          }
+          },
         });
         return;
+      }
+
+      // Rate limit (if configured)
+      if (rateLimit) {
+        const identifier = requestIp.getClientIp(req);
+        const result = await rateLimit.limit(identifier!);
+        res.setHeader("X-RateLimit-Limit", result.limit);
+        res.setHeader("X-RateLimit-Remaining", result.remaining);
+
+        if (!result.success) {
+          res
+            .status(429)
+            .json(
+              "Too many uploads in 1 minute. Please try again in a few minutes.",
+            );
+          return;
+        }
       }
 
       const prompt = prepareImagePromptForRequest(userDataObject.prompt, true);
@@ -55,15 +72,15 @@ export default async function handler(
     } catch (error) {
       res.status(500).json({
         error: {
-          message: 'An error occurred during your request.',
-        }
+          message: "An error occurred during your request.",
+        },
       });
     }
   } else {
     res.status(500).json({
       error: {
-        message: 'Method not supported',
-      }
+        message: "Method not supported",
+      },
     });
   }
 }
