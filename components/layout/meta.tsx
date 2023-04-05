70683622cf11cd70724cdb782d1ae5d97c079904
Kevin Haxhi <kevin.haxhi@softup.co>
2023-04-05 15:39:32 +0200

    Added new logo and favicon

--- components/layout/meta.tsx
+++ components/layout/meta.tsx
@@ -13,7 +13,7 @@ export default function Meta({
   return (
     <Head>
       <title>{title}</title>
-      <link rel="icon" href="/favicon.svg" />
+      <link rel="icon" href="/favicon.png" />
 
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
