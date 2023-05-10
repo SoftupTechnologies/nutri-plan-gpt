b28a495582432f7c8d60462cf4a552101c36ce33
Emrulla Ramilli <emrulla.ramilli@softup.co>
2023-05-10 15:13:14 +0200

    added lazy loading for carousel images and removed timeout for showing charts

--- components/shared/Carousel.tsx
+++ components/shared/Carousel.tsx
@@ -1,19 +1,24 @@
-import Image from 'next/image';
-import React from 'react';
-import {
-  Swiper,
-  SwiperSlide,
-} from 'swiper/react';
-import { Autoplay } from 'swiper';
-import { CarouselImage } from '@/lib/types';
+import Image from "next/image";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import { CarouselImage } from "@/lib/types";
+import cn from "classnames";
 
 interface Props {
-  images:CarouselImage[]
+  images: CarouselImage[];
 }
-const Carousel:React.FC<Props> = ({images}) => {
-  
+const Carousel: React.FC<Props> = ({ images }) => {
+  const [imagesData, setImagesData] = useState(() =>
+    images.map((image, index) => ({
+      index,
+      loaded: false,
+      imageUrl: image.imageUrl,
+    })),
+  );
+
   return (
-    <aside className="w-[300px] md:w-[500px] mx-auto">
+    <aside className="mx-auto w-[300px] md:w-[500px]">
       <Swiper
         slidesPerView={1}
         centeredSlides
@@ -22,23 +27,35 @@ const Carousel:React.FC<Props> = ({images}) => {
           delay: 1500,
           disableOnInteraction: false,
         }}
-        modules={[ Autoplay]}
+        modules={[Autoplay]}
         rewind
         className="mx-auto"
       >
-        {images.map((image) => {
-          return (
-            <SwiperSlide key={image.imageUrl}>
-              <Image
-                className="object-cover"
-                src={image.imageUrl}
-                width={600}
-                height={600}
-                alt="meal image"
-              />
-            </SwiperSlide>
-          );
-        })}
+        {imagesData.map((imageData, index) => (
+          <SwiperSlide key={imageData.index}>
+            <Image
+              width={600}
+              height={600}
+              alt="meal image"
+              src={imageData.imageUrl}
+              onLoadingComplete={() => setImagesData((currentImagesData) => {
+                const newImagesData = [...currentImagesData];
+                newImagesData[index] = {
+                  ...currentImagesData[index],
+                  loaded: true,
+                };
+
+                return newImagesData;
+              })}
+              className={cn(
+                "object-cover duration-700 ease-in-out group-hover:opacity-75",
+                !imageData.loaded
+                  ? "scale-110 blur-2xl grayscale"
+                  : "scale-100 blur-0 grayscale-0",
+              )}
+            />
+          </SwiperSlide>
+        ))}
       </Swiper>
     </aside>
   );
